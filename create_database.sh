8ae0ebbfc3e683aa10967d3ad3d52acf307f39f9
GINGFREES <lucian168unity@gmail.com>
2022-01-03 17:14:30 +0800

    add sh file for regenerate database and code

---
+++ create_database.sh
@@ -0,0 +1,32 @@
+MODEL_LIST=("Building" \
+"BuildingLevel" \
+"BuildingLevelSize" \
+"BuildingStyle" \
+"BuildingAnimSetting" \
+"Character" \
+"Diary" \
+"DiaryTitle" \
+"Island" \
+"Story" \
+"StoryDialogue")
+
+cd google-sheet-api-service
+
+# rm -rf Migrations/*
+# for((i=0; i<${#MODEL_LIST[@]};i++))
+# do
+#     echo ${MODEL_LIST[i]}
+#     MVC_CONTEXT_NAME="Mvc${MODEL_LIST[i]}Context"
+
+#     rm -rf ${MVC_CONTEXT_NAME}*.db
+#     dotnet ef migrations add InitialCreate --context ${MVC_CONTEXT_NAME}
+#     dotnet ef database update --context ${MVC_CONTEXT_NAME}
+# done
+
+MODEL_NAME="BuildingAnimSetting"
+rm -rf Migrations/Mvc${MODEL_NAME}*
+MVC_CONTEXT_NAME="Mvc${MODEL_NAME}Context"
+
+rm -rf ${MVC_CONTEXT_NAME}*.db
+dotnet ef migrations add InitialCreate --context ${MVC_CONTEXT_NAME}
+dotnet ef database update --context ${MVC_CONTEXT_NAME}
---
+++ create_module.sh
@@ -0,0 +1,38 @@
+MODEL_LIST=("Building" \
+"BuildingLevel" \
+"BuildingLevelSize" \
+"BuildingStyle" \
+"BuildingAnimSetting" \
+"Character" \
+"Diary" \
+"DiaryTitle" \
+"Island" \
+"Story" \
+"StoryDialogue")
+
+# dotnet-aspnet-codegenerator controller -name MoviesController -m Movie -dc MvcMovieContext --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries
+
+cd google-sheet-api-service
+# rm -rf Migrations/*
+# for((i=0; i<${#MODEL_LIST[@]};i++))
+# do
+#     echo ${MODEL_LIST[i]}
+#     CONTROLLER_NAME="${MODEL_LIST[i]}Controller"
+#     MVC_CONTEXT_NAME="Mvc${MODEL_LIST[i]}Context"
+
+#     rm -rf Controllers/${CONTROLLER_NAME}.cs \
+#     Data/${MVC_CONTEXT_NAME}.cs \
+#     Views/${MODEL_LIST[i]}*
+
+#     dotnet-aspnet-codegenerator controller -name ${CONTROLLER_NAME} -m ${MODEL_LIST[i]} -dc ${MVC_CONTEXT_NAME} --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries -sqlite
+# done
+
+MODEL_NAME="BuildingAnimSetting"
+rm -rf Migrations/Mvc${MODEL_NAME}*
+CONTROLLER_NAME="${MODEL_NAME}Controller"
+MVC_CONTEXT_NAME="Mvc${MODEL_NAME}Context"
+
+rm -rf Controllers/${CONTROLLER_NAME}.cs \
+Data/${MVC_CONTEXT_NAME}.cs \
+Views/${MODEL_NAME}*
+dotnet-aspnet-codegenerator controller -name ${CONTROLLER_NAME} -m ${MODEL_NAME} -dc ${MVC_CONTEXT_NAME} --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries -sqlite
